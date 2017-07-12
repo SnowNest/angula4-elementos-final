73925e9229a97adac229a1af1df4f2aec9d0b94a
como-quesito <rapidclimate@outlook.com>
2017-07-12 09:52:50 -0500

    gggg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {Mensaje} from './mensaje';
 
 @Component({
-  selector: 'app-root',
+  selector: 'principal',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  nombre:string='Juan Carlos';
+  mensa:Mensaje;
+  constructor(){
+    this.mensa=new Mensaje('primer','Mensajito');
+  }
 }
